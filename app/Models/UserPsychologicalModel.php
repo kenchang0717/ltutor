3f347379a04d13e5505a7ef5130ce2d532cc930a
LUNGTENG\3385 <kenchang@ltedu.com.tw>
2025-07-30 12:11:10 +0800

    ADD

---
+++ app/Models/UserPsychologicalModel.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Models;
+
+use CodeIgniter\Model;
+use Config\Database;
+use CodeIgniter\I18n\Time;
+
+class UserPsychologicalModel extends Model
+{
+    protected $table      = 'user_psychological';
+    protected $primaryKey = 'id';
+    protected $allowedFields = ['uid','email','is_register','created_at']; // 可插入欄位
+    // protected $useTimestamps = true;       // 如果表中有 created_at / updated_at 欄位
+
+    public function add(int $uid,string $email,int $is_register)
+    {
+        $data = [
+            'uid'  => $uid,
+            'email' => $email,
+            'is_register' => $is_register,
+            'created_at'  => Time::now('UTC')->toDateTimeString(),
+        ];
+
+        $this->insert($data);
+        return true;
+    }
+
+    public function checkEmailExist(string $email)
+    {
+        $data = $this->select('*')
+                    ->where('email', $email)
+                    ->find();
+
+        if(count($data)==0)
+            return $email;
+    }
+}
+
+?>
